863da05e576c922db7f5f984391d098c3cb02067
Rickyngotho <gicheric14@gmail.com>
2018-12-12 17:04:26 +0300

    modification

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
-from .models import Pic
+from .models import pic
 
 
 # Create your views here.

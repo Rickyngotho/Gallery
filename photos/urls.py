b00510eb1ce25d9c868df4ec62834e390e06e832
Rickyngotho <gicheric14@gmail.com>
2018-12-12 13:05:29 +0300

    deletion

--- photos/urls.py
+++ photos/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     url('^$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     url(r'^search/', views.search_results, name='search_results')
-    url(r'^article/(\d+)',views.article,name ='article')
+    url(r'^pic/(\d+)',views.Pic,name ='pic')
 
 ]
 if settings.DEBUG:

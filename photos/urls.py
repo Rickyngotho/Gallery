a4cf15d3162c4ff360f32571dc0e7f94f3be1c5d
Rickyngotho <gicheric14@gmail.com>
2018-12-12 17:02:01 +0300

    modification of urls

--- photos/urls.py
+++ photos/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
     url('^$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
     url(r'^pic/(\d+)',views.Pic,name ='pic')
 
 ]
